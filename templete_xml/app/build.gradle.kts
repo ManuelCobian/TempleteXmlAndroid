6f84c767725a53df7b493a5f26b8bf955ee16527
ManuelCobian <lcobianh@gmail.com>
2024-07-02 01:15:34 -0600

    templete add basic dependences gradle 8.6

---
+++ templete_xml/app/build.gradle.kts
@@ -0,0 +1,85 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
+}
+
+android {
+    namespace = "com.softing.templete_xml"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.softing.templete_xml"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
+    //Dagger Hitl
+    implementation(libs.androidx.hitl.kotlin)
+
+    //LIVE DATA
+    implementation(libs.androidx.live.data)
+
+    //NAVIGATION
+    implementation(libs.androidx.navigate.ui)
+    implementation(libs.androidx.navigate.fragment)
+
+    //GLIDE
+    implementation(libs.androidx.glide)
+
+    //CARDVIEW
+    implementation(libs.androidx.card.view)
+
+    //VIEW MODEL
+    implementation(libs.androidx.view.model)
+
+    //RETRFIT
+    implementation(libs.androidx.retrofit)
+
+    //GSON CONVERT
+    implementation(libs.androidx.retrofit.gson)
+    implementation(libs.androidx.retrofit.gson.conver)
+
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+kapt {
+    correctErrorTypes = true
+}
